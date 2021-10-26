3929496700ba0954fc07965d1a142a10deebb139
mdbappymia <mbm.21.02.16@gmail.com>
2021-10-27 02:38:08 +0600

    online school server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -71,6 +71,10 @@ const run = async () => {
   }
 };
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(port, () => {
   console.log("server is running");
